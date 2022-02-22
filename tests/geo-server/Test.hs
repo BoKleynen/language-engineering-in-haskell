19e0b994075664424cd7c2b362011213128b07fd
Bo Kleynen <kleynenbo@gmail.com>
2022-02-22 22:00:42 +0100

    Add basic tests

---
+++ tests/geo-server/Test.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Test.Hspec
+import Test.Tasty
+import Test.Tasty.Hspec
+
+import GeoServer
+
+main :: IO ()
+main = do
+    specs <- concat <$> mapM testSpecs
+        [ spec_intersection ]
+    defaultMain (testGroup "All tests" [
+        testGroup "Specs" specs
+     ])
+
+spec_intersection :: Spec
+spec_intersection =
+    describe "(/\\)" $ do
+        let
+            o = Point 0 0
+            c1 = circle 2
+
+        it "intersection with with outside" $
+            o `inRegion` (c1 /\  outside c1) `shouldBe` False
+
+        it "intersection with itself" $
+            o `inRegion` (c1 /\ c1) `shouldBe` True

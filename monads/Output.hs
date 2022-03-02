c943400d092410cd08df19c129c798bb024cea25
Bo Kleynen <kleynenbo@gmail.com>
2022-03-02 17:55:12 +0100

    Add examples from class

--- monads/Output.hs
+++ monads/Output.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE InstanceSigs #-}
 
 module Output where
+
 import Control.Monad (liftM2)
+import Control.Monad.Writer
+import Data.Monoid
+
 
 type Output = String
 
@@ -23,33 +27,40 @@ line :: Term -> Int -> Output
 line t a = "eval (" ++ show t ++ ") <=" ++ show a ++ "\n"
 
 
-newtype OutputM o a = MkOutput (o, a)
+newtype OutputM o a = Output (o, a)
 
 instance Functor (OutputM o) where
   fmap :: (a -> b) -> OutputM o a -> OutputM o b
-  fmap g (MkOutput (o, a)) = MkOutput (o, g a)
+  fmap g (Output (o, a)) = Output (o, g a)
 
 instance Monoid o => Applicative (OutputM o) where
   pure :: a -> OutputM o a
-  pure a = MkOutput (mempty, a)
+  pure a = Output (mempty, a)
 
   (<*>) :: OutputM o (a -> b) -> OutputM o a -> OutputM o b
-  (MkOutput (o1, g)) <*> (MkOutput (o2, x)) = MkOutput (o1 <> o2, g x)
+  (Output (o1, g)) <*> (Output (o2, x)) = Output (o1 <> o2, g x)
 
 instance Monoid o => Monad (OutputM o) where
   return :: a -> OutputM o a
   return = pure
 
   (>>=) :: OutputM o a -> (a -> OutputM o b) -> OutputM o b
-  (MkOutput (o, x)) >>= g = MkOutput (o <> o', b)
+  (Output (o, x)) >>= g = Output (o <> o', b)
     where
-      MkOutput (o', b) = g x
+      Output (o', b) = g x
 
 out :: Output -> OutputM Output ()
-out x = MkOutput (x, ())
+out x = Output (x, ())
 
 evalM :: Term -> OutputM Output Int
 evalM (Con a)   = out (line (Con a) a) >> return a
 evalM (Div t u) = do
   q <- liftM2 div (evalM t) (evalM u)
   out (line (Div t u) q) >> return q
+
+fac :: Int -> Writer (Sum Int) Int
+fac 0 = return 1
+fac n = do
+  tell 1
+  x <- fac (n-1)
+  return (n*x)
--- monads/State.hs
+++ monads/State.hs
@@ -19,40 +19,59 @@ eval (Div t u) x =
   in  (a `div` b, z + 1)
 
 
-newtype StateM a = MkState (State -> (a, State))
+newtype StateM s a = State (s -> (a, s))
 
-instance Functor StateM where
-  fmap :: (a -> b) -> StateM a -> StateM b
-  fmap g (MkState x) = MkState (
-    \s -> let (x', s') = x s
-          in (g x', s')
-    )
+runState :: s -> StateM s a -> (a,s)
+runState s (State m) = m s
 
-instance Applicative StateM where
-  pure :: a -> StateM a
-  pure x = MkState (\s -> (x, s))
+instance Functor (StateM s) where
+  fmap :: (a -> b) -> StateM s a -> StateM s b
+  fmap g x = x >>= \a -> return (g a)
+  -- fmap g (State x) = State (
+  --   \s0 -> let (x', s1) = x s0
+  --          in (g x', s1)
+  --   )
 
-  (<*>) :: StateM (a -> b) -> StateM a -> StateM b
-  (MkState g) <*> (MkState x) = MkState (
-    \s -> let (g', s') = g s
-              (x', s'') = x s'
-          in (g' x', s'')
-    )
+instance Applicative (StateM s) where
+  pure :: a -> StateM s a
+  pure x = State (\s -> (x, s))
 
-instance Monad StateM where
-  return :: a -> StateM a
+  (<*>) :: StateM s (a -> b) -> StateM s a -> StateM s b
+  g <*> x = g >>= \f -> x >>= \y -> return (f y)
+  -- (State g) <*> (State x) = State (
+  --   \s0 -> let (g', s1) = g s0
+  --              (x', s2) = x s1
+  --         in (g' x', s2)
+  --   )
+
+instance Monad (StateM s) where
+  return :: a -> StateM s a
   return = pure
 
-  (>>=) :: StateM a -> (a -> StateM b) -> StateM b
-  (MkState x) >>= g = MkState (
-    \s -> let (x', s') = x s
-              MkState y = g x'
-          in y s'
+  (>>=) :: StateM s a -> (a -> StateM s b) -> StateM s b
+  (State x) >>= g = State (
+    \s0 -> let (x', s1) = x s0
+               State y = g x'
+           in y s1
     )
 
-tick :: StateM ()
-tick = MkState (\s -> ((), s + 1))
+get :: StateM s s
+get = State (\s -> (s, s))
 
-evalM :: Term -> StateM Int
+put :: s -> StateM s ()
+put s = State (const ((), s))
+
+tick :: StateM Int ()
+tick = State (\s -> ((), s + 1))
+
+evalM :: Term -> StateM Int Int
 evalM (Con a) = return a
 evalM (Div t u) = tick >> liftM2 div (evalM t) (evalM u)
+
+numbers :: [a] -> StateM Int [(Int,a)]
+numbers []     = pure []
+numbers (x:xs) = do
+  n <- get
+  put (n+1)
+  ys <- numbers xs
+  return ((n,x) : ys)
